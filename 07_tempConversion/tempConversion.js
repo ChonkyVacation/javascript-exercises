7ea0dff2d3dfc99121f27500c12d7f0e2b803696
ChonkyVacation <chris.mclaren1@hotmail.com>
2022-10-07 22:20:04 +1100

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = (x1) => {
+  let x2 = ((x1 - 32) * 5/9 )
+  return (Math.round(x2 * 10) / 10);
 
 };
 
-const ctof = function() {
-
+const ctof = function(y1) {
+  let y2 = (y1 * 9/5 + 32);
+  return (Math.round(y2 * 10) / 10);
 };
 
 // Do not edit below this line
